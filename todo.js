2f9adebe46d4ad006307d8ce6fc2fe64594b34f5
M-Adil-code <muhammadadeelkhan29@gmail.com>
2025-08-18 20:44:41 +0500

    Fixing Bugs

--- todo.js
+++ todo.js
@@ -1,53 +1,89 @@
-let add = document.querySelector('input')
-
 let copy;
 let input = document.querySelector('input')
 
 let list = document.querySelector('.LIST>ul')
 let arrtode = new Array;
 let storageitems;
-if (localStorage.getItem(1) !== null) {
+
+
+// Showing localStorage Todoes
+if (localStorage.getItem(1) !== null || localStorage.getItem(1)==[]) {
     arrtode = JSON.parse(localStorage.getItem(1))
     storageitems = JSON.parse(localStorage.getItem(1))
     storageitems.forEach(e => {
         additem(e)
     })
 }
+// clear all item logic
 clear.addEventListener('click', () => {
     let result = confirm('Do You Want To Process?')
     if (result) {
         localStorage.clear()
         list.innerHTML = ''
+        arrtode=[]
     }
 })
+
+// Send item to additem() function and filter also
 
 input.addEventListener('keypress',
     function (e) {
         if (e.key == 'Enter') {
-            let inputtxt = add.value
-            let presenttodo = JSON.parse(localStorage.getItem(1))
-            console.log(presenttodo, presenttodo == null)
-            if (presenttodo == null) {
-                additem(inputtxt)
-                arrtode.push(inputtxt)
-                localStorage.setItem(1, JSON.stringify(arrtode))
-            }
-            else {
-                if (!presenttodo.includes(inputtxt.trim())) {
-                    additem(inputtxt)
-                    arrtode.push(inputtxt)
-
-                    localStorage.setItem(1, JSON.stringify(arrtode))
-                }
-                else {
-                    alert('Item Alredy Exists!')
-
-                }
-            }
+            filterItem(input.value)
+            input.value=''
+            
         }
     }
 
 )
+
+let filterItem=((prompt)=>{
+    console.log("At Filter Function : ",prompt)
+
+    // Already Stored item in localStorage
+    let presenttodo = JSON.parse(localStorage.getItem(1))
+    // dublicatechecker(prompt)
+
+    if(dublicatechecker(prompt) ){
+        console.log(dublicatechecker(prompt))
+    }else{
+        alert('item Already exists')
+    }
+    if(prompt.trim()==''){
+        alert('Please Enter any Task')
+    }
+
+    //item addition
+    if(dublicatechecker(prompt)&& prompt.trim()!==''){
+        itemsendtolocalstorage(prompt)
+        additem(prompt)
+    }
+
+    
+
+
+})
+let dublicatechecker=((prompt)=>{
+    let  presenttodo = JSON.parse(localStorage.getItem(1))
+    console.log('Already Stored item in localStorage',presenttodo)
+    
+    if (presenttodo !== null) {
+         if(presenttodo.includes(prompt.trim())){
+            return false;
+         }else{
+            return true
+         }
+            
+    }else{
+        return true;
+    }       
+})
+
+let itemsendtolocalstorage=((item)=>{
+    arrtode.push(item)
+    localStorage.setItem(1, JSON.stringify(arrtode))
+})
+
 function additem(value) {
 
     let listitem = document.createElement('li')
